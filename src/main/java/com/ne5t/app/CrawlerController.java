9dc2370c00b68a4bb6459ce74b9b021658e9d137
Nuno Esteves <nesteves@users.noreply.github.com>
2015-12-30 23:59:44 +0000

    Set up the constructor and general runCrawler loop

--- src/main/java/com/ne5t/app/CrawlerController.java
+++ src/main/java/com/ne5t/app/CrawlerController.java
@@ -1,11 +1,47 @@
 package com.ne5t.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+import com.ne5t.app.crawler.Crawler;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.LinkedList;
+
+public class CrawlerController {
+    
+    private int maxPagesToVisit;
+    private Set<String> pagesVisited;
+    private List<String> pagesToVisit;
+    private Crawler crawler;
+    
+    /**
+     * Initialize the basic instance variables
+     *  the maximum number of pages to visit
+     *  a list of the unique urls already visited
+     *  a list of urls found by the crawler and not yet
+     * visited
+     */
+    public CrawlerController(int maxPagesToVisit, String startingPageUrl) {
+        this.maxPagesToVisit = maxPagesToVisit;
+        pagesVisited = new HashSet<String>();
+        pagesToVisit = new LinkedList<String>();
+
+        pagesToVisit.add(startingPageUrl);
+
+        this.crawler = new Crawler();
+    }
+
+    /**
+     * Method that calls the Crawler and PageFetcher classes
+     * while there are pages to visit or the maximum number
+     * of pages has not been reached
+     */
+    private void runCrawler() {
+        
+        while (pagesVisited.size() < maxPagesToVisit && !pagesTiVisit.isEmpty()) {
+            
+        }
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
